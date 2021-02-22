bcb76d7fc37404f25babdb2bae6c469ebd058ce5
BoSswosile <yohan.moren@gmail.com>
2021-02-22 11:38:24 +0100

    try

--- resources/views/admin/nvPhoto.blade.php
+++ resources/views/admin/nvPhoto.blade.php
@@ -5,6 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1>Mettre en ligne une photo</h1>
+            <form>
+            <label for ="desc">Description de l'image :</label><br>
+            <input type ="text" id="desc" name ="desc" value=""><br>
+            </form>
         </div>
     </div>
 </div>
