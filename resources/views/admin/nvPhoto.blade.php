2328225644481432f76575efb19cb594c41181e4
BoSswosile <yohan.moren@gmail.com>
2021-02-22 12:58:28 +0100

    champs

--- resources/views/admin/nvPhoto.blade.php
+++ resources/views/admin/nvPhoto.blade.php
@@ -6,10 +6,39 @@
         <div class="col-md-8">
             <h1>Mettre en ligne une photo</h1>
             <form method="POST">
-            <label for ="desc">Description de l'image :</label><br>
-            <input type ="text" id="desc" name ="desc" value=""><br>
-            <label for ="takenby">Pris avec :</label><br>
-            <input type ="text" id="takenby" name ="takenby" value=""><br>
+            @csrf
+            <label for ="nom">Nom :</label><br>
+            <input type ="text" id="nom" name ="nom" value=""><br><br>
+            <label for ="type">Type de photo :</label><br>
+            <input type ="text" id="type" name ="type" value=""><br><br>
+            <label for="product">Le produit est-il sur la photo ?</label><br>
+            <label for="oui">Oui</label>&nbsp
+            <input type ="radio" id="oui" name="product" value="oui"><br>
+            <label for="non">Non</label>
+            <input type ="radio" id="non" name="product" value="non"><br>
+            <label for="product">Une personne est-elle présente sur la photo ?</label><br>
+            <label for="oui">Oui</label>&nbsp
+            <input type ="radio" id="oui" name="humain" value="oui"><br>
+            <label for="non">Non</label>
+            <input type ="radio" id="non" name="humain" value="non"><br>
+            <label for="product">Cette photo a-t-elle été prise dans un but institutionnel?</label><br>
+            <label for="oui">Oui</label>&nbsp
+            <input type ="radio" id="oui" name="institutionnelle" value="oui"><br>
+            <label for="non">Non</label>
+            <input type ="radio" id="non" name="institutionelle" value="non"><br>
+            <label for ="credits">Détenteur de la photo :</label><br>
+            <input type ="text" id="credits" name ="credits" value=""><br><br>
+            <label for="product">Les droits d'utilisation de cette photo sont ils limités ?</label><br>
+            <label for="oui">Oui</label>&nbsp
+            <input type ="radio" id="oui" name="droitsLimites" value="oui"><br>
+            <label for="non">Non</label>
+            <input type ="radio" id="non" name="droitsLimites" value="non"><br>
+            <label for="cp">Image sous copyright</label>&nbsp
+            <input type ="checkbox" id="cp" name="cp" value="copyright"><br>
+            <label for ="datefindroits">Date de fin de droits (JJ/MM/YYYY) :</label><br>
+            <input type ="text" id="datefindroits" name ="datefindroits" value=""><br><br>
+            <label for ="tags">Tags :</label><br>
+            <input type ="text" id="tags" name ="tags" value=""><br><br>
             <input type ="submit">
             </form>
         </div>
