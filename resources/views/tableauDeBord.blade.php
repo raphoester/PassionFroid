aea401f768adc51d20eeaccea59a0f536d971a8c
BoSswosile <yohan.moren@gmail.com>
2021-02-23 16:04:15 +0100

    one of the last

--- resources/views/tableauDeBord.blade.php
+++ resources/views/tableauDeBord.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
+<style>
+.register{
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+    margin-top: 3%;
+    padding: 3%;
+}</style>
+<div class="jumbotron">
+  <h1 class="display-4">Bienvenue sur le site de PassionFroid !</h1>
+  <p class="lead">Ce site aura pour but de partager des images de nos produits ainsi que des images d'ambiances.</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Commencez par voir les images présentes sur le site</a>
+  </p>
+</div>
 @endsection
